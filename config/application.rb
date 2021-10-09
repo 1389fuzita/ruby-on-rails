9be725eb110dbeae1f0d8e15b764385625efbc20
EC2 Default User <ec2-user@ip-172-31-12-199.ec2.internal>
2021-10-09 21:51:03 +0000

    [Finish 2-7]

--- config/application.rb
+++ config/application.rb
@@ -17,3 +17,4 @@ module SampleApp
     # the framework and any gems in your application.
   end
 end
+
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '2b6a6852221f6bbddf62afcddd653a6b4487d10375c6852a283cf19dbc4b188ce5209102b1b814ef1b34e0b0b963436f6b7da8a87ec3f6b201740dbdd4ba7a70'
